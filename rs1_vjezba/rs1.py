38538b83631531b483532434815965f047503d4a
KBazina <kbazina@unipu.hr>
2025-10-27 15:26:56 +0100

    prvi rs1

---
+++ rs1_vjezba/rs1.py
@@ -0,0 +1,28 @@
+# print("unesi prvi broj: ")
+# a = int(input())
+# print("unesi drugi broj: ")
+# b = int(input())
+
+# znak = input()
+
+# match znak:
+#     case "+":
+#         print(a," plus ",b, " = ",a+b)
+#     case "-":
+#         print(a," minus ",b, " = ",a-b)
+#     case "*":
+#         print (a," puta ",b, " = ",a*b)
+#     case "/":
+#         print(a," kroz ",b, " = ",a/b)
+#     case _:
+#         print("unesi tocan znak")
+
+# print("unesi godinu")
+# a=int(input())
+
+# if a % 400 ==0:
+#     print("godina " , a, " je prijestupna")
+# elif a % 4 == 0 and a % 100 != 0:
+#     print("godina " , a, " je prijestupna")
+# else:
+#     print("godina " , a, " nije prijestupna")
